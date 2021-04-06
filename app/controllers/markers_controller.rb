019633cbbe320a248332dbf4fcf83b607962a0ba
david aravena <david.aravenap@gmail.com>
2021-04-05 23:59:24 -0400

    este si es el fin

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -9,6 +9,13 @@ class MarkersController < ApplicationController
 
   end
 
+  def show
+    @marker_categories = MarkerCategory.find(params[:id])
+    @marker = Marker.new
+    @category = Category.new
+    @marker_category = MarkerCategory.new
+  end
+
   def create
     if params[:marker]
       @marker = Marker.new(marker_params)
@@ -31,9 +38,34 @@ class MarkersController < ApplicationController
     end
   end
 
+
+  def update
+    if params[:marker]
+      @marker = Marker.find(params[:id])
+      @marker.update(marker_params_edit)
+      redirect_to root_path
+    elsif params[:category]
+      @category = Category.find(params[:id])
+      @category.update(category_params)
+      redirect_to root_path
+    elsif params[:marker_category]
+      @marker_category = MarkerCategory.find(params[:id])
+      @marker_category.update(marker_category_params)
+      redirect_to root_path
+    end
+  end
+
+
+
   def categories_endpoint
     @categories = Category.all
     render json: @categories.to_json
+  end
+
+  def destroy
+    @marker = MarkerCategory.find(params[:id])
+    @marker.destroy
+    redirect_to root_path
   end
 
   private
@@ -41,9 +73,14 @@ class MarkersController < ApplicationController
   def marker_params
     params.require(:marker).permit(:url)
   end
+  def marker_params_edit
+    params.require(:marker).permit(:url, :id)
+  end
 
+
+  
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, :id)
   end
 
   def marker_category_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'markers#index'
   post 'markers/create', to: 'markers#create', as: 'm_c'
+  get 'markers/show/:id', to: 'markers#show', as: 'm_s'
+  patch 'markers/update/:id', to: 'markers#update', as: 'm_u'
 
   get 'categories', to: 'markers#categories_endpoint', as: 'm_c_e'
+
+  delete 'markers/delete/:id', to: 'markers#destroy', as: 'm_d'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
